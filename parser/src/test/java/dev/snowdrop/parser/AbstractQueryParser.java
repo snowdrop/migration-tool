bae1a414ddd60a915d9ae3485a5f4bb3ec2db204
cmoulliard <cmoulliard@redhat.com>
2025-10-15 16:28:19 +0200

    Add parser test case
    Signed-off-by: cmoulliard <cmoulliard@redhat.com>

---
+++ parser/src/test/java/dev/snowdrop/parser/AbstractQueryParser.java
@@ -0,0 +1,29 @@
+package dev.snowdrop.parser;
+
+import dev.snowdrop.parser.antlr.QueryLexer;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+public abstract class AbstractQueryParser {
+    QueryVisitor parseQuery(String query) {
+        try {
+            ANTLRInputStream input = new ANTLRInputStream(query);
+            QueryLexer lexer = new QueryLexer(input);
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+            dev.snowdrop.parser.antlr.QueryParser parser = new dev.snowdrop.parser.antlr.QueryParser(tokens);
+            ParseTree tree = parser.searchQuery();
+
+            // Create and use the visitor
+            QueryVisitor visitor = new QueryVisitor();
+            visitor.visit(tree);
+
+            return visitor;
+        } catch (Exception e) {
+            System.err.println("Error parsing query: " + query);
+            e.printStackTrace();
+            return new QueryVisitor();
+        }
+    }
+}
---
+++ parser/src/test/java/dev/snowdrop/parser/QueryParserTest.java
@@ -0,0 +1,39 @@
+package dev.snowdrop.parser;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.wildfly.common.Assert;
+
+import java.util.Map;
+import java.util.Set;
+
+public class QueryParserTest extends AbstractQueryParser{
+    @Test
+    public void simpleQuery() {
+        String simpleQuery = "FIND java.annotation WHERE (name='@SpringBootApplication')";
+        QueryVisitor visitor = parseQuery(simpleQuery);
+
+        // Don't include simple quotes around the key or value
+        Query query = new Query("java","annotation", Map.of("name", "@SpringBootApplication"));
+
+        Set<Query> queries = visitor.getSimpleQueries();
+        Assert.assertTrue(queries.size() == 1);
+        Assertions.assertEquals(queries.stream().findFirst().get(), query);
+    }
+
+    @Test
+    public void queryWithAnd() {
+        String queryWithAnd = "FIND java.annotation WHERE (name='@SpringBootApplication') AND pom.dependency WHERE (artifactId='quarkus-core', version='3.16.2')";
+        QueryVisitor visitor = parseQuery(queryWithAnd);
+
+        // Don't include simple quotes around the key or value
+        Query queryA = new Query("java","annotation", Map.of("name", "@SpringBootApplication"));
+        Query queryB = new Query("pom","dependency", Map.of("artifactId", "quarkus-core","version", "3.16.2"));
+
+        Set<Query> queries = visitor.getAndQueries();
+        var queryList = queries.stream().toList();
+        Assert.assertTrue(queryList.size() == 2);
+        Assertions.assertEquals(queryList.get(0),queryA);
+        Assertions.assertEquals(queryList.get(1),queryB);
+    }
+}
