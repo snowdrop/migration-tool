17f35e183d2960f3ac725c366dc12e86d8894eeb
cmoulliard <cmoulliard@redhat.com>
2025-10-29 12:52:03 +0100

    Add new tests and create separate test classes. #58
    Signed-off-by: cmoulliard <cmoulliard@redhat.com>
    
    # Conflicts:
    #	parser/src/test/java/dev/snowdrop/parser/QueryWithAndOperatorTest.java

---
+++ parser/src/test/java/dev/snowdrop/parser/QueryWithOrOperatorTest.java
@@ -0,0 +1,28 @@
+package dev.snowdrop.parser;
+
+import dev.snowdrop.model.Query;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.wildfly.common.Assert;
+
+import java.util.Map;
+import java.util.Set;
+
+public class QueryWithOrOperatorTest extends AbstractQueryParser{
+
+    @Test
+    public void queryWithOr() {
+        String queryWithOr = "java.annotation is '@SpringBootApplication' OR java.annotation is '@Deprecated' ";
+        QueryVisitor visitor = parseQuery(queryWithOr);
+
+        // Don't include simple quotes around the key or value
+        Query queryA = new Query("java","annotation", Map.of("name", "@SpringBootApplication"));
+        Query queryB = new Query("java","annotation", Map.of("name", "@Deprecated"));
+
+        Set<Query> queries = visitor.getOrQueries();
+        var queryList = queries.stream().toList();
+        Assert.assertTrue(queryList.size() == 2);
+        Assertions.assertEquals(queryList.get(0),queryA);
+        Assertions.assertEquals(queryList.get(1),queryB);
+    }
+}
---
+++ parser/src/test/java/dev/snowdrop/parser/SimpleQueryTest.java
@@ -0,0 +1,51 @@
+package dev.snowdrop.parser;
+
+import dev.snowdrop.model.Query;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.wildfly.common.Assert;
+
+import java.util.Map;
+import java.util.Set;
+
+public class SimpleQueryTest extends AbstractQueryParser{
+    @Test
+    public void clauseWithSingleQuotes() {
+        String simpleQuery = "java.annotation is '@SpringBootApplication'";
+        QueryVisitor visitor = parseQuery(simpleQuery);
+
+        // Don't include simple quotes around the key or value
+        Query query = new Query("java","annotation", Map.of("name", "@SpringBootApplication"));
+
+        Set<Query> queries = visitor.getSimpleQueries();
+        Assert.assertTrue(queries.size() == 1);
+        Assertions.assertEquals(queries.stream().findFirst().get(), query);
+    }
+
+    @Test
+    public void clauseWithoutKeyValuePairs() {
+        String simpleQuery = "java.annotation is '@SpringBootApplication'";
+        QueryVisitor visitor = parseQuery(simpleQuery);
+
+        // Should automatically use "name" as default key for annotation
+        Query expectedQuery = new Query("java","annotation", Map.of("name", "@SpringBootApplication"));
+
+        Set<Query> queries = visitor.getSimpleQueries();
+        Assert.assertTrue(queries.size() == 1);
+        Assertions.assertTrue(queries.contains(expectedQuery));
+    }
+
+
+    @Test
+    public void clauseWithDoubleQuotes() {
+        String annotationQuery = "java.annotation is \"@SpringBootApplication\"";
+        QueryVisitor visitor = parseQuery(annotationQuery);
+
+        // Should automatically use "name" as default key for annotation
+        Query expectedQuery = new Query("java","annotation", Map.of("name", "@SpringBootApplication"));
+
+        Set<Query> queries = visitor.getSimpleQueries();
+        Assert.assertTrue(queries.size() == 1);
+        Assertions.assertTrue(queries.contains(expectedQuery));
+    }
+}
