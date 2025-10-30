1a65605502e7cd0ec35c0bad63d537aa61b85a43
Aurea Munoz <devauriverse@gmail.com>
2025-10-30 17:08:54 +0100

    add spring-autoconfigure to the parent pom and rename the parent reference

--- migration-cli/src/test/java/dev/snowdrop/analyze/render/AsciiTableWithHyperlink.java
+++ migration-cli/src/test/java/dev/snowdrop/analyze/render/AsciiTableWithHyperlink.java
@@ -24,19 +24,30 @@ public class AsciiTableWithHyperlink {
         System.out.println(createLink("https://google.com", "Google me"));
 
         List<String[]> tableData = new ArrayList<>();
-        tableData.add(new String[]{"000-springboot-annotation-notfound", "springboot -> quarkus", "No", "No match found"});
-        tableData.add(new String[]{"001-springboot-replace-bom-quarkus","springboot -> quarkus","Yes","Found SpringBootApplication at line 6, char: 1 - 22\nfile:///Users/cmoullia/code/application-modernisation/migration-tool-parent/applications/demo-spring-boot-todo-app/src/main/java/com/todo/app/AppApplication.java"});
-        tableData.add(new String[]{"002-springboot-add-class-quarkus","springboot -> quarkus","Yes","Found SpringBootApplication at line 6, char: 1 - 22\nfile:///Users/cmoullia/code/application-modernisation/migration-tool-parent/applications/demo-spring-boot-todo-app/src/main/java/com/todo/app/AppApplication.java"});
-        tableData.add(new String[]{"003-springboot-to-quarkusmain-annotation","springboot -> quarkus","Yes","Found SpringBootApplication at line 6, char: 1 - 22\nfile:///Users/cmoullia/code/application-modernisation/migration-tool-parent/applications/demo-spring-boot-todo-app/src/main/java/com/todo/app/AppApplication.java"});
-        tableData.add(new String[]{"004-springboot-to-quarkus-rest-annotations","springboot -> quarkus","Yes","Found PostMapping at line 77, char: 5 - 17\nfile:///Users/cmoullia/code/application-modernisation/migration-tool-parent/applications/demo-spring-boot-todo-app/src/main/java/com/todo/app/controller/TaskController.java\n--- rewrite ---\nFound ResponseBody at line 77, char: 5 - 17\nfile:///Users/cmoullia/code/application-modernisation/migration-tool-parent/applications/demo-spring-boot-todo-app/src/main/java/com/todo/app/controller/TaskController.java"});
+        tableData.add(
+                new String[] { "000-springboot-annotation-notfound", "springboot -> quarkus", "No", "No match found" });
+        tableData.add(new String[] { "001-springboot-replace-bom-quarkus", "springboot -> quarkus", "Yes",
+                "Found SpringBootApplication at line 6, char: 1 - 22\nfile:///Users/cmoullia/code/application-modernisation/migration-tool-parent/applications/demo-spring-boot-todo-app/src/main/java/com/todo/app/AppApplication.java" });
+        tableData.add(new String[] { "002-springboot-add-class-quarkus", "springboot -> quarkus", "Yes",
+                "Found SpringBootApplication at line 6, char: 1 - 22\nfile:///Users/cmoullia/code/application-modernisation/migration-tool-parent/applications/demo-spring-boot-todo-app/src/main/java/com/todo/app/AppApplication.java" });
+        tableData.add(new String[] { "003-springboot-to-quarkusmain-annotation", "springboot -> quarkus", "Yes",
+                "Found SpringBootApplication at line 6, char: 1 - 22\nfile:///Users/cmoullia/code/application-modernisation/migration-tool-parent/applications/demo-spring-boot-todo-app/src/main/java/com/todo/app/AppApplication.java" });
+        tableData.add(new String[] { "004-springboot-to-quarkus-rest-annotations", "springboot -> quarkus", "Yes",
+                "Found PostMapping at line 77, char: 5 - 17\nfile:///Users/cmoullia/code/application-modernisation/migration-tool-parent/applications/demo-spring-boot-todo-app/src/main/java/com/todo/app/controller/TaskController.java\n--- rewrite ---\nFound ResponseBody at line 77, char: 5 - 17\nfile:///Users/cmoullia/code/application-modernisation/migration-tool-parent/applications/demo-spring-boot-todo-app/src/main/java/com/todo/app/controller/TaskController.java" });
 
         System.out.println("\n=== Migration data with Styler ===");
-        var asciiTable = AsciiTable.builder().styler(customizeStyle()).data(tableData, Arrays.asList(
-                 new Column().header("Rule ID").headerAlign(HorizontalAlign.LEFT).dataAlign(HorizontalAlign.LEFT).with(r -> r[0]),
-                 new Column().header("Source to Target").headerAlign(HorizontalAlign.LEFT).dataAlign(HorizontalAlign.LEFT).with(r -> r[1]),
-                 new Column().header("Match").headerAlign(HorizontalAlign.CENTER).dataAlign(HorizontalAlign.CENTER).with(r -> r[2]),
-                 new Column().header("Information Details").headerAlign(HorizontalAlign.LEFT).maxWidth(120).dataAlign(HorizontalAlign.LEFT).with(r -> r[3])
-            )).asString();
+        var asciiTable = AsciiTable.builder().styler(customizeStyle())
+                .data(tableData,
+                        Arrays.asList(
+                                new Column().header("Rule ID").headerAlign(HorizontalAlign.LEFT)
+                                        .dataAlign(HorizontalAlign.LEFT).with(r -> r[0]),
+                                new Column().header("Source to Target").headerAlign(HorizontalAlign.LEFT)
+                                        .dataAlign(HorizontalAlign.LEFT).with(r -> r[1]),
+                                new Column().header("Match").headerAlign(HorizontalAlign.CENTER)
+                                        .dataAlign(HorizontalAlign.CENTER).with(r -> r[2]),
+                                new Column().header("Information Details").headerAlign(HorizontalAlign.LEFT)
+                                        .maxWidth(120).dataAlign(HorizontalAlign.LEFT).with(r -> r[3])))
+                .asString();
         System.out.println(asciiTable);
     }
 }
