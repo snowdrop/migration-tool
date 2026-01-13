14267a5526398b407e1da46928ae66b6e8ba18b8
Charles Moulliard <cmoulliard@ibm.com>
2026-01-13 10:25:08 +0100

    Use the latest released jar. Should be externalized. #193
    Signed-off-by: Charles Moulliard <cmoulliard@ibm.com>

--- scanner/src/main/java/dev/snowdrop/mtool/scanner/openrewrite/OpenRewriteQueryScanner.java
+++ scanner/src/main/java/dev/snowdrop/mtool/scanner/openrewrite/OpenRewriteQueryScanner.java
@@ -109,7 +109,7 @@ public class OpenRewriteQueryScanner implements QueryScanner {
 		// Recipe's jar files
 		String gavs = "org.openrewrite:rewrite-java:8.71.0,"
 				+ "org.openrewrite.recipe:rewrite-java-dependencies:1.49.0,"
-				+ "dev.snowdrop.mtool:openrewrite-recipes:1.0.5";
+				+ "dev.snowdrop.mtool:openrewrite-recipes:1.0.4";
 		Map<String, Map<String, String>> recipeMap = (Map<String, Map<String, String>>) first;
 
 		return recipeMap.values().iterator().next();
@@ -205,7 +205,7 @@ public class OpenRewriteQueryScanner implements QueryScanner {
 		// Recipe's jar files
 		String gavs = "org.openrewrite:rewrite-java:8.65.0,"
 				+ "org.openrewrite.recipe:rewrite-java-dependencies:1.44.0,"
-				+ "dev.snowdrop.mtool:openrewrite-recipes:1.0.0-SNAPSHOT";
+				+ "dev.snowdrop.mtool:openrewrite-recipes:1.0.4";
 
 		try {
 			List<String> command = new ArrayList<>();
