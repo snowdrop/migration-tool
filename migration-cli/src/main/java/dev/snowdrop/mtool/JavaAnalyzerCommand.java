0559ce305005e4a808dc25d213efd0b024d07d20
Aurea Munoz <devauriverse@gmail.com>
2026-03-18 15:48:21 +0100

    Add version provider for main command

--- migration-cli/src/main/java/dev/snowdrop/mtool/JavaAnalyzerCommand.java
+++ migration-cli/src/main/java/dev/snowdrop/mtool/JavaAnalyzerCommand.java
@@ -7,6 +7,15 @@ import picocli.CommandLine;
 
 @TopCommand
 @CommandLine.Command(name = "mtool", description = "Quarkus mtool client able to scan, analyze and migrate a java application using instructions", subcommands = {
-		AnalyzeCommand.class, TransformCommand.class, CommandLine.HelpCommand.class})
+		AnalyzeCommand.class, TransformCommand.class,
+		CommandLine.HelpCommand.class}, versionProvider = JavaAnalyzerCommand.VersionProvider.class, mixinStandardHelpOptions = true)
 public class JavaAnalyzerCommand {
+
+	static class VersionProvider implements CommandLine.IVersionProvider {
+		@Override
+		public String[] getVersion() {
+			String version = JavaAnalyzerCommand.class.getPackage().getImplementationVersion();
+			return new String[]{"mtool " + (version != null ? version : "unknown")};
+		}
+	}
 }
